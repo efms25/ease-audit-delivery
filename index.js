130bc43d476cd543a91d28960c9408eb70067697
Eric Santana <efms.king@gmail.com>
2026-09-12 16:35:31 -0300

    :sparkles: feat: add db connection

---
+++ index.js
@@ -0,0 +1,2 @@
+const connection = require('./src/database/connection.js');
+
---
+++ src/database/connection.js
@@ -0,0 +1,29 @@
+const { Pool } = require("pg");
+
+const dbAccess = {
+  POSTGRES_HOST: "localhost",
+  POSTGRES_DB: "ease_queue_db",
+  POSTGRES_USER: "ease_queue_user",
+  POSTGRES_PASSWORD: "ease_queue_pass",
+};
+
+const connection = (function () {
+  let client;
+
+  if (!client) {
+    client = new Pool({
+      host: dbAccess.POSTGRES_HOST,
+      database: dbAccess.POSTGRES_DB,
+      user: POSTGRES_USER,
+      password: dbAccess.POSTGRES_PASSWORD,
+    });
+  }
+
+  return {
+    client: client,
+  };
+})();
+
+module.exports = {
+  connection,
+};
